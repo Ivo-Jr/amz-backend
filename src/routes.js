5d1941dc8f1f9f30e1c4da2e7b6caf6bd074e6c5
Jose Ivo <Jose@Bizs-MBP.bizsys.local>
2021-01-22 17:01:55 -0300

    upload images local

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/teste', (request, response) => {
-    return response.json({ message: 'Hello Júnior!'})
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
+
+    return res.json({ message: 'Hello Júnior!'})
 });
 
 module.exports = routes;
