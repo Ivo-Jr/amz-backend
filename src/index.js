77d4ceea4bf6e7d66e654dd43c6e0ac19d79e553
Ivo-Jr <maciel.jr95@gmail.com>
2021-01-27 16:26:59 -0300

    mongodb compass running

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mogoose = require('mongoose');
 
 const app = express();
+
+mogoose.connect(
+    "mongodb+srv://bizsys_totem:j56tZpGLCjozDxvH@mongo-bizsys.av1f6.mongodb.net/Amazon_teste",
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology: true
+    }
+)
 
 // Express conseguirá lidar com as requisições com o corpo de mensagem vindo com o formato de json.
 app.use(express.json());
@@ -9,9 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-
 app.use(require('./routes'));
-
 
 app.listen(8000, () => {
     console.log('Server running! 🚀')
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,20 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
-routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file);
+const Post = require('./models/Post');
 
-    return res.json({ message: 'Hello Júnior!'})
+routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
+    const { originalname: name, size, filename: key} = req.file
+    
+    const post = await Post.create({
+        name,
+        size,
+        key,
+        url: ""
+    })
+
+
+    return res.json(post)
 });
 
 module.exports = routes;
